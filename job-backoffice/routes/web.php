726473ec16c6ed93cdcf76c92d62f3a7e96796da
mohamedibrahimabdulghani <mohamedibrahimabdulghani@gmail.com>
2026-03-15 17:57:50 +0200

    Refactor controllers, update routes, and enhance navigation UI
    - Created new controllers for modular functionality.
    - Linked views to the corresponding controllers.
    - Updated web.php to reflect the new routes and navigation structure.
    - Redesigned the navigation bar for improved layout and user experience.

--- job-backoffice/routes/web.php
+++ job-backoffice/routes/web.php
@@ -1,14 +1,24 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\JobVacancyController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/company', [CompanyController::class, 'index'])->name('company.index');
+    Route::get('/job_application', [JobApplicationController::class, 'index'])->name('job_application.index');
+    Route::get('/job_category', [JobCategoryController::class, 'index'])->name('job_category.index');
+    Route::get('/job_vacancy', [JobVacancyController::class, 'index'])->name('job_vacancy.index');
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
